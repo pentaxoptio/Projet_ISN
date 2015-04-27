d98f7c332ebf9cac09cf0612b812ff87694bbdd1
valentin <valentin.rigolle@gmail.com>
2015-04-27 14:12:03 +0200

    Supprimé 4 lignes inutiles dans le constructeur

--- MainMenuState.cpp
+++ MainMenuState.cpp
@@ -4,11 +4,6 @@ MainMenuState::MainMenuState(StateStack &stack, Context context) :
 	State(stack, context)
 	, m_bg(context.textures->getTexture(TitleScreen))
 {
-	sf::Vector2f scale(
-			(float)context.window->getSize().x / m_bg.getLocalBounds().width,
-			(float)context.window->getSize().y / m_bg.getLocalBounds().height);
-	m_bg.setScale(scale);
-
 	gui::Button* play = new gui::Button(false, "Play", context.fonts->get(Default), *context.textures);
 	play->setPosition(100, 300);
 	play->setCallback([this] ()
